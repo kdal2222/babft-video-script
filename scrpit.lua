78f6d134746c3a358ecfb613ea7d623f999579e9
kdal2222 <kam10upup@gmail.com>
2025-11-02 21:59:09 +0300

    Update scrpit.lua

--- scrpit.lua
+++ scrpit.lua
@@ -1,8 +1,8 @@
-# скрипт от rd450x
-# прога на конвертацию видео/фото в код для скрипта: https://github.com/kdal2222/babft-video-script/blob/main/convert.py
-# минимальное количество пластика: x * y
-# картника или видео строятся от блока дерева (видео идет вврх и влево от блока)
-# перед запуском скрипта не должны стоять блоки пластика и должен стоять 1 блок дерева
+-- скрипт от rd450x
+-- прога на конвертацию видео/фото в код для скрипта: https://github.com/kdal2222/babft-video-script/blob/main/convert.py
+-- минимальное количество пластика: x * y
+-- картника или видео строятся от блока дерева (видео идет вврх и влево от блока)
+-- перед запуском скрипта не должны стоять блоки пластика и должен стоять 1 блок дерева
 
 local Players = game:GetService("Players")
 local Http = game:GetService("HttpService")
@@ -13,29 +13,29 @@ local ResizeCounter = 0
 
 print('running')
 
-# НАСТРОЙКИ
+-- НАСТРОЙКИ
 
-local x = 64 # Ширина видео/фото
-local y = 21 # Высота видео/фото
-local pr = 1 # плотность пикселей на блок (сломано, значение не менять)
+local x = 64 -- Ширина видео/фото
+local y = 21 -- Высота видео/фото
+local pr = 1 -- плотность пикселей на блок (сломано, значение не менять)
 local fps = 10
-local url = "https://raw.githubusercontent.com/kdal2222/babft-video-script/refs/heads/main/BADAPPLE64X21/" # ссылка на видео / фото
-local frames = 3285 # количество кадров в видео (ставь 0 если фото)
-local plastic_count = 22500 # количество блоков пластика в инвенторе
+local url = "game:HttpGet('https://raw.githubusercontent.com/kdal2222/babft-video-script/refs/heads/main/BADAPPLE64X21/" --- ссылка на видео / фото
+local frames = 3285 -- количество кадров в видео (ставь 0 если фото)
+local plastic_count = 22500 -- количество блоков пластика в инвенторе
 
-# КОНЕЦ НАСТРОЕК
+-- КОНЕЦ НАСТРОЕК
 
 local pixels = {}
 local video = {}
 
-for i = 0, frames do # переносит переделанное видео в массив video
-	table.insert(video, Http:JSONDecode(game:HttpGet('url..frame_'.. i ..'.json')))
+for i = 0, frames do -- переносит переделанное видео в массив video
+	table.insert(video, Http:JSONDecode(url..frame_'.. i ..'.json')))
 	print(i)
 end
 
-local start = os.time() # запускает таймер (необязателен)
+local start = os.time() -- запускает таймер (необязателен)
 
-local function clear_table(tablee) # фиксит список pixels (мб не нужно)
+local function clear_table(tablee) -- фиксит список pixels (мб не нужно)
 	local new_table = {}
 	for i, v in ipairs(tablee) do
 		if i <= x * pr * y * pr then
@@ -46,7 +46,7 @@ local function clear_table(tablee) # фиксит список pixels (мб не
 	return new_table
 end
 
-local function create_pixels() # дает блокам положение X и Y
+local function create_pixels() -- дает блокам положение X и Y
 	local blocks = workspace.Blocks[player.Name]
 	local main = blocks.WoodBlock
 
@@ -67,7 +67,7 @@ local function create_pixels() # дает блокам положение X и Y
 end
 
 local AutoResize
-AutoResize = workspace.Blocks[player.Name].ChildAdded:Connect(function(child) # изменяет размер блоков
+AutoResize = workspace.Blocks[player.Name].ChildAdded:Connect(function(child) -- изменяет размер блоков
 	local args = {
 		child,
 		vector.create(1 / pr, 1 / pr, 0.5),
@@ -76,7 +76,7 @@ AutoResize = workspace.Blocks[player.Name].ChildAdded:Connect(function(child) # 
 	player.Character:WaitForChild("ScalingTool").RF:InvokeServer(unpack(args))
 	ResizeCounter += 1
 	
-	# print(ResizeCounter, x * pr * y * pr) для отладки
+	-- print(ResizeCounter, x * pr * y * pr) для отладки
 
 	if ResizeCounter >= x * pr * y * pr - 1 then
 		ResizeCounter = 0
@@ -95,8 +95,8 @@ AutoResize = workspace.Blocks[player.Name].ChildAdded:Connect(function(child) # 
 			local frame_done = false
 			
 			for _, v in ipairs(pixels) do
-				player.Character:WaitForChild("PaintingTool", 9999999999999) # ожидает кисточку в руках
-				local index = (v['Y'] * x * pr) + (v['X'] + 1) # вычисляет индекс в списке video по X и Y
+				player.Character:WaitForChild("PaintingTool", 9999999999999) -- ожидает кисточку в руках
+				local index = (v['Y'] * x * pr) + (v['X'] + 1) -- вычисляет индекс в списке video по X и Y
 				task.spawn(function()
 					local args = {
 						{
@@ -119,10 +119,10 @@ AutoResize = workspace.Blocks[player.Name].ChildAdded:Connect(function(child) # 
 				wait()
 			until frame_done
 			
-			# print(1 / fps - ((DateTime.now().UnixTimestampMillis - fps_fix) / 1000)) для отладки, показывает задержку кадра (если время положитьельное будет фпс который указан, 
-			# минусовое значение показыват сколько времяни в секундах стоит кадр)
+			-- print(1 / fps - ((DateTime.now().UnixTimestampMillis - fps_fix) / 1000)) для отладки, показывает задержку кадра (если время положитьельное будет фпс который указан, 
+			-- минусовое значение показыват сколько времяни в секундах стоит кадр)
 
-			wait(1 / fps - ((DateTime.now().UnixTimestampMillis - fps_fix) / 1000)) # fps
+			wait(1 / fps - ((DateTime.now().UnixTimestampMillis - fps_fix) / 1000)) -- fps
 		end
 	end
 end)
@@ -131,7 +131,7 @@ local countt = 0
 
 for i = 1, x, 1 / pr do
 	for j = 1, y, 1 / pr do
-		task.spawn(function() # генерация блоков
+		task.spawn(function() -- генерация блоков
 			local args = {
 				"PlasticBlock",
 				plastic_count,
